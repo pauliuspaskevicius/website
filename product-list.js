3a62f74f7dbaf9f5d8b8a30cdc8d33d00e26f1a6
Paulius Paškevičius <paulius.paskevicius@gmail.com>
2016-07-22 13:05:35 -0300

    Added search

--- product-list.js
+++ product-list.js
@@ -1,6 +1,7 @@
 var display = getQueryString("display");
 
 var container = document.getElementById("product-list");
+var search = document.getElementById("product-search");
 var displayList = document.getElementById("display-list");
 var displayThumbnails = document.getElementById("display-thumbnails");
 
@@ -19,39 +20,58 @@ function onDataLoad (e) {
   }
 }
 
+function search (text) {
+  var results = [];
+  for (var i=0; i<items.length; i++) {
+    if (items[i].name == text) {
+      results.push(items[i]);
+    }
+  }
+  render(results);
+}
+
+search.addEventListener("keyup", function (e) {
+  search(e.target.value);
+  console.log(e);
+});
+
 displayList.addEventListener("click", function () {
-  renderList ();
+  renderList (items);
 });
 
 displayThumbnails.addEventListener("click", function () {
-  renderThumbnails ();
+  renderThumbnails (items);
 });
 
-function renderList () {
+function render (list) {
+  display == "list" ? renderList(list) : renderThumbnails(list);
+}
+
+function renderList (list) {
   container.innerHTML = null;
   container.className = "";
-  for (var i=0; i<items.length ;i++) {
+  for (var i=0; i<list.length; i++) {
     var item = document.createElement("li");
     var a = document.createElement("a");
-    a.href = "product.html?id=" + items[i].id;
-    a.innerText = items[i].name;
+    a.href = "product.html?id=" + list[i].id;
+    a.innerText = list[i].name;
     item.appendChild(a);
     container.appendChild(item);
   }
 }
 
-function renderThumbnails () {
+function renderThumbnails (list) {
   container.innerHTML = null;
   container.className = "products-thumbnail";
   container.innerHTML = null;
-  for (var i=0; i<items.length ;i++) {
+  for (var i=0; i<list.length; i++) {
     var item = document.createElement("li");
     var a = document.createElement("a");
-    a.href = "product.html?id=" + items[i].id;
+    a.href = "product.html?id=" + list[i].id;
     var img = document.createElement("img");
-    img.src = items[i].image;
+    img.src = list[i].image;
     var span = document.createElement("span");
-    span.innerText = items[i].name;
+    span.innerText = list[i].name;
     a.appendChild(img);
     a.appendChild(span);
     item.appendChild(a);
